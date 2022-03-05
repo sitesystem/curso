1f225862aaa122c674ad213adfb0ba9dd123cadf
sitesystem <l_aloaxa05@hotmail.com>
2022-03-05 12:02:00 -0600

    actualización

---
+++ Lógica de Programación/HolaMundo.py
@@ -0,0 +1,8 @@
+# COMENTARIO: Ejemplo Saludo
+# Los comentario sirven para documentar o describir
+# cada parte del código
+
+print("Hola Mundo")
+print("Eduardo")
+print('Janine')
+print(2*5)
---
+++ Lógica de Programación/OperacionesMatemáticas.py
@@ -0,0 +1,33 @@
+# IMPORTAR LA LIBRERÍA math: Funciones Matemáticas
+# SINTAXIS:
+import math
+
+# ENTRADA DE DATOS
+
+# Definir, crear o declarar Variables
+number1 = 10
+number2 = 2
+
+# PROCESOS (Cálculos u Operaciones Mátemáticas y Lógicas)
+suma = number1 + number2
+resta = number1 - number2
+mult = number1 * number2
+div = number1 / number2
+potencia1 = number1 ** number2
+potencia2 = pow(number1, number2)
+cuadrado = number1 ** 2
+cubo = pow(number1, 3)
+raíz_cuadrada1 = pow(number1, 1/2)
+raíz_cuadrada2 = math.sqrt(number1)
+raíz_cúbica = pow(number2, 1/3)
+módulo_residuo = number1 % number2
+
+# SALIDA DE DATOS
+print("La suma =", suma, "\nLa resta = ", resta)
+
+print('La suma = ' + str(suma)) # CONCATENACIÓN (Unir textos)
+# Convertir tipo de dato número a texto (Casteo)
+print(f"La suma = {suma}") # Interpolación de cadenas
+
+
+
